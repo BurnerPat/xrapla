7030585dbb899b9909c4c18fce825ebd62731c7c
markus.geissel <markus.geissel@googlemail.com>
2013-03-07 12:01:57 +0000

    Bean Tutor erstellen

---
+++ xrapla/src/org/xrapla/beans/Tutor.java
@@ -0,0 +1,33 @@
+package org.xrapla.beans;
+
+public class Tutor extends User {
+
+	private String title;
+	private String subject;
+	
+	
+	public Tutor(){
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getSubject() {
+		return subject;
+	}
+
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+}
+	
+	
