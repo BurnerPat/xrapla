150f756899d729555af839f3f8a3fc92792eaf39
c.meder1990@googlemail.com <c.meder1990@googlemail.com@516336f8-8a58-62a3-ea2e-662aac7b07db>
2013-03-07 11:53:42 +0000

    Added Beans: -Appointment -Course -Group -Lecture -User

---
+++ trunk/xrapla/src/org/xrapla/beans/Lecture.java
@@ -0,0 +1,52 @@
+package org.xrapla.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Lecture {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	Course course;
+	Docent docent;
+
+	public Lecture(){
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public Docent getDocent() {
+		return docent;
+	}
+
+	public void setDocent(Docent docent) {
+		this.docent = docent;
+	}
+
+}
---
+++ trunk/xrapla/src/org/xrapla/beans/User.java
@@ -0,0 +1,56 @@
+package org.xrapla.beans;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int username;	
+	private String password;
+	private String lastname;
+	private String firstname;
+	private String email;
+	
+	public User(){
+	}
+	
+	public int getUsername() {
+		return username;
+	}
+	public void setId(int username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString(){
+		return username + ": " + lastname + ", " + firstname;
+	}	
+	
+}
