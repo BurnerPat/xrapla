9128029f84d925295e405303d3e2a551700c9a1f
annikamyr@googlemail.com <annikamyr@googlemail.com@516336f8-8a58-62a3-ea2e-662aac7b07db>
2013-03-07 12:13:05 +0000

    Warnungen beheben

--- trunk/xrapla/src/org/xrapla/beans/Appointment.java
+++ trunk/xrapla/src/org/xrapla/beans/Appointment.java
@@ -2,9 +2,6 @@ package org.xrapla.beans;
 
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Appointment {
