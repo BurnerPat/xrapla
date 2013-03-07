552cde8fbd4873f0fc441ca7a98fc7ea02719bd4
annikamyr@googlemail.com <annikamyr@googlemail.com@516336f8-8a58-62a3-ea2e-662aac7b07db>
2013-03-07 16:46:37 +0000

    Methode, die die Termine einer Woche liefert

--- trunk/xrapla/src/org/xrapla/classes/AppointmentProvider.java
+++ trunk/xrapla/src/org/xrapla/classes/AppointmentProvider.java
@@ -1,5 +1,53 @@
 package org.xrapla.classes;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.xrapla.beans.*;
+
 public class AppointmentProvider {
+		
+	@SuppressWarnings("unchecked")
+	public List<Appointment> getAppointments(int weekOfYear, int year)
+	{			    
+	    Calendar monday = new GregorianCalendar();	    
+	    monday.setWeekDate(year, weekOfYear, Calendar.MONDAY);	   	
+	    	    
+	    Calendar sunday = monday;
+	    sunday.add(Calendar.DATE, 7);	
+//		return getAppointments(monday, sunday);
+//	}
+//	
+//	
+//	private List<Appointment> getAppointments(Calendar monday, Calendar sunday){		
+		EntityManagerFactory factory;		 
+	    factory = Persistence.createEntityManagerFactory("Appointment");
+	    EntityManager em = factory.createEntityManager();	       	    
+	    
+	    // Build and execute SQL-Statement
+	    Query q = em.createQuery(
+	    		"SELECT * " +
+	    		"FROM Appointment" +
+	    		"WHERE date BETWEEN " + 
+	    				calendarToSql(monday) +
+    				" AND " +
+    					calendarToSql(sunday));	   
+    	List<Appointment> appointments = q.getResultList();
+    	
+	    return appointments;
+	}
+	
+	private String calendarToSql(Calendar day)	
+	{		
+		return day.get(Calendar.YEAR) + "-" +
+				day.get(Calendar.MONTH) + "-" +
+				day.get(Calendar.DATE); 
+	}
 
 }
