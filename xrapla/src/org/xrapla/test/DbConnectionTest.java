eb69d905ce132ba5106be7d9a29115b21ab6acba
annikamyr <annikamyr@googlemail.com>
2013-03-08 18:05:52 +0000

    Berichtigung

--- xrapla/src/org/xrapla/test/DbConnectionTest.java
+++ xrapla/src/org/xrapla/test/DbConnectionTest.java
@@ -8,7 +8,8 @@ import org.xrapla.classes.AppointmentProvider;
 public class DbConnectionTest {
 
 	public static void main(String[] args) {
-		List<Appointment> appointments = new AppointmentProvider().getAppointments(5, 2013);
+		List<Appointment> appointments = new AppointmentProvider().getAppointments(1, 2013);
+		System.out.println("Termine(" + appointments.size() + "):");
 		for(Appointment ap : appointments)
 			System.out.println(ap);
 	}
