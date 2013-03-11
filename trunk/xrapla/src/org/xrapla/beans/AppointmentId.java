88c376db9f1e0e704cdeac636b165cd09805f710
annikamyr@googlemail.com <annikamyr@googlemail.com@516336f8-8a58-62a3-ea2e-662aac7b07db>
2013-03-11 14:35:01 +0000

    Datenbankkommunikationsfehler beheben

--- trunk/xrapla/src/org/xrapla/beans/AppointmentId.java
+++ trunk/xrapla/src/org/xrapla/beans/AppointmentId.java
@@ -3,12 +3,28 @@ package org.xrapla.beans;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.xrapla.beans.Appointment;
+
+//@Embeddable
 public class AppointmentId implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	/*@ManyToOne
+	@JoinColumn(name="ROOM_ID")	*/
 	private Room room;
+	
+	//@Temporal(TemporalType.DATE)
 	private Date date;
+	//@Temporal(TemporalType.TIME)
 	private Date time;
 	
 	public AppointmentId() {
