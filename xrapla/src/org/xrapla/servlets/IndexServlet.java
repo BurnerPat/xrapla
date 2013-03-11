2ef995807a99196722271f328ad13e602e685b03
burner.patrick <burner.patrick@googlemail.com>
2013-03-11 10:25:08 +0000

    - Updated welcome page - Update index servlet - Remove unused imports

--- xrapla/src/org/xrapla/servlets/IndexServlet.java
+++ xrapla/src/org/xrapla/servlets/IndexServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/index.html")
+@WebServlet("/index.jsp")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
