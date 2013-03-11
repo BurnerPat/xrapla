dafeec441b837275e1bdfed8b11b9093f1356103
annikamyr <annikamyr@googlemail.com>
2013-03-11 11:18:02 +0000

    Room Klasse erstellt

---
+++ xrapla/src/org/xrapla/beans/Room.java
@@ -0,0 +1,27 @@
+package org.xrapla.beans;
+
+import javax.persistence.Id;
+
+public class Room {
+	
+	@Id
+	int number;
+		
+	char wing;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public char getWing() {
+		return wing;
+	}
+
+	public void setWing(char wing) {
+		this.wing = wing;
+	}	
+}
