bb5e985933f08e5c0638193b391e1478f60a8a74
annikamyr@googlemail.com <annikamyr@googlemail.com@516336f8-8a58-62a3-ea2e-662aac7b07db>
2013-03-12 10:11:02 +0000

    überflüssige Imports gelöscht

--- trunk/xrapla/src/org/xrapla/classes/RoomProvider.java
+++ trunk/xrapla/src/org/xrapla/classes/RoomProvider.java
@@ -8,22 +8,21 @@ import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
 import org.xrapla.Constants;
-import org.xrapla.beans.Appointment;
 import org.xrapla.beans.Room;
 
 public class RoomProvider {
-	public List<Room> getRooms(){
-		EntityManagerFactory factory;		 
-	    factory = Persistence.createEntityManagerFactory(Constants.PERSISTANCE_UNIT_NAME);
-	    EntityManager em = factory.createEntityManager();	       	    
-	    
-	    // Build and execute SQL-Statement
-	    TypedQuery<Room> q = em.createQuery(
-	    		"SELECT r " +
-	    		"FROM Room r", Room.class);
-	    	    
-    	List<Room> rooms = q.getResultList();
-    	
-	    return rooms;
+	public List<Room> getRooms() {
+		EntityManagerFactory factory;
+		factory = Persistence
+				.createEntityManagerFactory(Constants.PERSISTANCE_UNIT_NAME);
+		EntityManager em = factory.createEntityManager();
+
+		// Build and execute SQL-Statement
+		TypedQuery<Room> q = em.createQuery("SELECT r " + "FROM Room r",
+				Room.class);
+
+		List<Room> rooms = q.getResultList();
+
+		return rooms;
 	}
 }
