dd8065c32e48dee0d9c098b1e489ed8e85e35358
annikamyr <annikamyr@googlemail.com>
2013-03-12 15:05:15 +0000

    Differenzierung des Attributs course

--- xrapla/src/org/xrapla/beans/Course.java
+++ xrapla/src/org/xrapla/beans/Course.java
@@ -2,10 +2,10 @@ package org.xrapla.beans;
 
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -26,7 +26,7 @@ public class Course {
 	@OneToMany(mappedBy="course")
 	private List<CourseGroup> groups;
 	
-	@OneToMany(mappedBy="course")
+	@OneToMany(mappedBy="relatedCourse")
 	private List<Student> students;
 	
 	public Course(){
