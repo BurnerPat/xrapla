5932a80859eb85ff033ecff3bacf9d6ca4e525db
annikamyr@googlemail.com <annikamyr@googlemail.com@516336f8-8a58-62a3-ea2e-662aac7b07db>
2013-03-13 13:34:49 +0000

    Umwandlung in UnitTest

--- trunk/xrapla/src/org/xrapla/test/AppointmentProviderTest.java
+++ trunk/xrapla/src/org/xrapla/test/AppointmentProviderTest.java
@@ -1,6 +1,13 @@
 package org.xrapla.test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -9,6 +16,7 @@ import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
+import org.junit.Test;
 import org.xrapla.beans.Appointment;
 import org.xrapla.beans.Room;
 import org.xrapla.beans.Student;
@@ -17,80 +25,123 @@ import org.xrapla.classes.AppointmentProvider;
 
 public class AppointmentProviderTest {
 
-	public static void main(String[] args) {
-		
+	private User getTestUser() {
+		EntityManagerFactory factory;
+		factory = Persistence.createEntityManagerFactory("xrapla");
+		EntityManager em = factory.createEntityManager();
+
+		TypedQuery<User> q = em.createQuery("SELECT a " + "FROM User a "
+				+ "WHERE TYPE(a) = ?1", User.class);
+
+		q.setParameter(1, Student.class);
+
+		List<User> users;
+		try {
+			users = q.getResultList();
+		} catch (NoResultException ex) {
+			users = new ArrayList<User>();
+		}
+
+		for (User us : users) {
+			if (us instanceof Student) {
+				em.close();
+				return us;
+			}
+		}
+
+		em.close();
+		return null;
+	}
+
+	private Room getRoom() {
+		EntityManagerFactory factory;
+		factory = Persistence.createEntityManagerFactory("xrapla");
+		EntityManager em = factory.createEntityManager();
+
+		TypedQuery<Room> q = em.createQuery(
+				"SELECT a " + 
+				"FROM Room a " +
+				"WHERE a.number = ?1", Room.class);
+
+		q.setParameter(1, 474);
+
+		List<Room> rooms;
+		try {
+			rooms = q.getResultList();
+		} catch (NoResultException ex) {
+			rooms = new ArrayList<Room>();
+		}
+
+		em.close();
+		return rooms.size() == 0 ? null : rooms.get(0);
+	}
+
+	@Test
+	public void testAppointmentForWeek() {
 		AppointmentProvider prov = new AppointmentProvider();
-		
-		EntityManagerFactory factory;		 
-	    factory = Persistence.createEntityManagerFactory("xrapla");
-	    EntityManager em = factory.createEntityManager();
-	    TypedQuery<User> q = em.createQuery(
-	    		"SELECT a " +
-	    		"FROM User a " +
-	    		"WHERE TYPE(a) = ?1", User.class);
-	    
-	    q.setParameter(1, Student.class);
-	    	
-	    List<User> users;
-	    try{
-	    	users = q.getResultList();
-	    }catch(NoResultException ex){
-	    	users = new ArrayList<User>();
-	    }
-	    
-	    Student user = null;
-	    for(User us : users){
-	    	if(us instanceof Student){
-	    		user = (Student) us;
-	    		break;
-	    	}
-	    }
-		
-		// Termine pro Woche
-		List<Appointment> appointmentsPerUser = prov.getAppointments(1, 2013, user);		
-		
-		// nächsten Termine für alle User					    		
+		Calendar cal = new GregorianCalendar();
+
+		User user = getTestUser();
+
+		List<Appointment> appointmentsPerUser = prov.getAppointments(
+				cal.get(Calendar.WEEK_OF_YEAR), cal.get(Calendar.YEAR), user);
+
+		for (Appointment appointment : appointmentsPerUser) {
+			Calendar calApp = new GregorianCalendar();
+			calApp.setTime(appointment.getDate());
+			assertThat(calApp.get(Calendar.WEEK_OF_YEAR),
+					is(cal.get(Calendar.WEEK_OF_YEAR)));
+		}
+	}
+
+	@Test
+	public void testAppointmentForWeekAndRoom() {
+		AppointmentProvider prov = new AppointmentProvider();
+		Calendar cal = new GregorianCalendar();
+
+		Room room = getRoom();
+
+		List<Appointment> appointmentsPerRoom = prov.getAppointments(
+				cal.get(Calendar.WEEK_OF_YEAR), cal.get(Calendar.YEAR), room);
+
+		for (Appointment appointment : appointmentsPerRoom) {
+			Calendar calApp = new GregorianCalendar();
+			calApp.setTime(appointment.getDate());
+			assertThat("Weekday:", calApp.get(Calendar.WEEK_OF_YEAR),
+					is(cal.get(Calendar.WEEK_OF_YEAR)));
+			assertTrue("Room:", appointment.getRoom().getNumber() == room.getNumber());
+		}
+
+	}
+
+	@Test
+	public void testNextTwoAppointments() {
+		AppointmentProvider prov = new AppointmentProvider();
+		User user = getTestUser();
+
 		List<Appointment> nextEvents = prov.getNextTwoAppointments(user);
-	    
-	    // Termine pro Woche pro Raum
-	    Room room = new Room();
-	    room.setNumber(474);
-	    room.setWing('A');
-	    List<Appointment> appointmentsPerRoom = prov.getAppointments(11, 2013, room);
-		
-		// Nächsten 3 Klausuren
-		List<Appointment> exams = prov.getExams(user);				
-		
-		// Anzeige
-		
-		System.out.println("Termine in Woche 1 des Jahres 2013 für " + user);
-		if(appointmentsPerUser != null){
-			for(Appointment ap : appointmentsPerUser)
-				System.out.println(ap);
+
+		for (Appointment appointment : nextEvents) {
+			assertTrue("Date: ",
+					appointment.getDate().getTime() >= new Date().getTime());
+			for (Student student : appointment.getGroup().getStudents())
+				assertThat("User: ", student, is(user));
 		}
-		
-		System.out.println();
-		
-		System.out.println("Nächsten 2 Termine für " + user);
-		if(appointmentsPerUser != null){
-			for(Appointment ap : nextEvents)
-				System.out.println(ap);
-		}
-		
-	    System.out.println();
-		
-		System.out.println("Termine in Woche 11 des Jahres 2013 in Raum " + room);
-		if(appointmentsPerUser != null){
-			for(Appointment ap : appointmentsPerRoom)
-				System.out.println(ap);
-		}
-		
-		System.out.println();
-		
-		System.out.println("Nächste Klausuren: ");
-		if(exams != null){
-			for(Appointment exam : exams)
-				System.out.println(exam);
+
+		assertTrue("Count: ", nextEvents.size() <= 2);
+	}
+
+	@Test
+	public void testNextExams() {
+		AppointmentProvider prov = new AppointmentProvider();
+		User user = getTestUser();
+
+		List<Appointment> exams = prov.getExams(user);
+		for (Appointment exam : exams) {
+			assertThat("Category: ", exam.getCategory(),
+					is(Appointment.CATEGORY_EXAM));
+			assertTrue("Date: ",
+					exam.getDate().getTime() >= new Date().getTime());
 		}
 	}
 }
