7796cd3658612d68ae4bba718590b741ae89290f
annikamyr@googlemail.com <annikamyr@googlemail.com@516336f8-8a58-62a3-ea2e-662aac7b07db>
2013-03-20 13:27:44 +0000

    Local Annotation

--- trunk/xrapla/src/org/xrapla/classes/UserProviderInterface.java
+++ trunk/xrapla/src/org/xrapla/classes/UserProviderInterface.java
@@ -1,7 +1,10 @@
 package org.xrapla.classes;
 
+import javax.ejb.Local;
+
 import org.xrapla.beans.User;
 
+@Local
 public interface UserProviderInterface {
 	public User getUser(String username, String password);	
 }
