7b88d6b1b5681d201a14b2fb017521be0d547fa4
annikamyr <annikamyr@googlemail.com>
2013-04-04 10:28:09 +0000

    test case that is true in any case to see whether jenkins works right

---
+++ xraplaEJB/ejbModule/org/xrapla/test/JenkinsTest.java
@@ -0,0 +1,15 @@
+package org.xrapla.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JenkinsTest {
+
+	@Test
+	public void test() {
+		assertTrue("Successful test",true);
+		//fail("Not yet implemented");
+	}
+
+}
