75a5fa4d77adc3db9856134ad25392919ab2f785
annikamyr <annikamyr@googlemail.com>
2013-04-07 17:51:52 +0000

    implement methods to get RoomProvider and RoomValitor through the BeanFactory

--- xraplaEJB/ejbModule/org/xrapla/factory/BeanFactory.java
+++ xraplaEJB/ejbModule/org/xrapla/factory/BeanFactory.java
@@ -3,47 +3,69 @@ package org.xrapla.factory;
 import javax.naming.InitialContext;
 
 import org.xrapla.sessionbean.AppointmentProviderLocal;
-import org.xrapla.sessionbean.UserProvider;
+import org.xrapla.sessionbean.RoomProviderLocal;
+import org.xrapla.sessionbean.RoomValidatorLocal;
 import org.xrapla.sessionbean.UserProviderLocal;
-
-/**
- * Session Bean implementation class BeanFactory
- */
 
 public class BeanFactory {
 
-    /**
-     * Default constructor. 
-     */
-    private BeanFactory() {
+	private BeanFactory() {
 
-    }
+	}
 
-    public static UserProviderLocal getUserProvider() {
-    	UserProviderLocal bean = null;
-    	
-        try {
-            InitialContext ctx = new InitialContext();
-            bean = (UserProviderLocal) ctx.lookup("java:global/xrapla/xraplaEJB/UserProvider!org.xrapla.sessionbean.UserProviderLocal");
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        
-        return bean;
-    }
-    
-    public static AppointmentProviderLocal getAppointmentProvider() {
-    	AppointmentProviderLocal bean = null;
-    	
-    	try {
-    		InitialContext ctx = new InitialContext();
-    		bean = (AppointmentProviderLocal) ctx.lookup("java:global/xrapla/xraplaEJB/AppointmentProvider!org.xrapla.sessionbean.AppointmentProviderLocal");
-    	}
-    	catch (Exception ex) {
-    		ex.printStackTrace();
-    	}
-    	
-    	return bean;
-    }
+	public static UserProviderLocal getUserProvider() {
+		UserProviderLocal bean = null;
+
+		try {
+			InitialContext ctx = new InitialContext();
+			bean = (UserProviderLocal) ctx
+					.lookup("java:global/xrapla/xraplaEJB/UserProvider!org.xrapla.sessionbean.UserProviderLocal");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return bean;
+	}
+
+	public static AppointmentProviderLocal getAppointmentProvider() {
+		AppointmentProviderLocal bean = null;
+
+		try {
+			InitialContext ctx = new InitialContext();
+			bean = (AppointmentProviderLocal) ctx
+					.lookup("java:global/xrapla/xraplaEJB/AppointmentProvider!org.xrapla.sessionbean.AppointmentProviderLocal");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return bean;
+	}
+
+	public static RoomProviderLocal getRoomProvider() {
+		RoomProviderLocal bean = null;
+
+		try {
+			InitialContext ctx = new InitialContext();
+			bean = (RoomProviderLocal) ctx
+					.lookup("java:global/xrapla/xraplaEJB/RoomProvider!org.xrapla.sessionbean.RoomProviderLocal");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return bean;
+	}
+
+	public static RoomValidatorLocal getRoomValidator() {
+		RoomValidatorLocal bean = null;
+
+		try {
+			InitialContext ctx = new InitialContext();
+			bean = (RoomValidatorLocal) ctx
+					.lookup("java:global/xrapla/xraplaEJB/RoomValidator!org.xrapla.sessionbean.RoomValidatorLocal");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return bean;
+	}
 }
